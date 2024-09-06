0e1cc24410ff7492817b38876d2684273abb16c9
Étienne Tremblay <2133318@etu.cchic.ca>
2024-09-06 16:02:23 -0400

    controlleue section commencé

---
+++ ForumDiscussion/ForumDiscussion/ViewModels/SectionListVM.cs
@@ -0,0 +1,13 @@
+﻿using ForumDiscussion.Models;
+
+namespace ForumDiscussion.ViewModels
+{
+    public class SectionListVM
+    {
+        public List<Section> Sections { get; }
+        public SectionListVM(List<Section> sections)
+        {
+            Sections = sections;
+        }
+    }
+}
